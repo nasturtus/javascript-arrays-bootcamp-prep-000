4cbbb0519c4252be646389c6f6ec8bf49314d781
Santosh SN <nasturtus@gmail.com>
2018-02-03 04:09:30 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, elem) {
+  var newArr = arr
+  newArr.unshift(elem)
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem) {
+  arr.unshift(elem);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, elem) {
+  var newArr = arr;
+  newArr.push(elem);
+  return newArr;
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem ) {
+  arr.push(elem);
+  return arr;
+}
+
+function accessElementInArray(arr, i) {
+  return arr[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  var sliced = arr.slice(1);
+  return sliced;
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  var sliced = arr.slice(0, arr.length - 1)
+  return sliced;
+}
